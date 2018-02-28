167a407e2286f2b693802996a4768556d0c6962d
TJ Kelly <tj.kelly0795@gmail.com>
2018-02-28 15:39:44 -0500

    The real init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace theCountBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTelegram().Wait();
+        }
+
+        static async Task RunTelegram()
+        {
+                var botClient = new Telegram.Bot.TelegramBotClient("564251024:AAH2xC9SeCOf7JuXpdZXifPJ0koAgzwOURo");
+                Telegram.Bot.Types.User me = await botClient.GetMeAsync();
+                System.Console.WriteLine($"Hello! My name is {me.FirstName}");
+
+                return;
+        }
+    }
+}
