eae4c7055b0ddb36d7b66f44b06a324c5306cc08
TJ Kelly <tj.kelly0795@gmail.com>
2018-03-01 18:27:03 -0500

    Getting ready for prod

--- TelegramBotManager.cs
+++ TelegramBotManager.cs
@@ -7,39 +7,69 @@ namespace TheCountBot
 {
     internal class TelegramBotManager
     {
-        ITelegramBotClient _botClient;
+        private ITelegramBotClient _botClient;
+
+        private int? lastNumber = null;
 
         internal TelegramBotManager()
         {
             _botClient = new TelegramBotClient( Settings.BotIdSecret );
 
             _botClient.OnMessage += OnMessageReceivedAsync;
-            _botClient.OnMessageEdited += OnMessageEditedRecievedAysnc;
+            //_botClient.OnMessageEdited += OnMessageEditedRecievedAysnc;
         }
 
-        internal void Startup()
+        internal async Task StartupAsync()
         {
+            await SendMessageAsync("Welcome me, heathens").ConfigureAwait(false);
             _botClient.StartReceiving();
         }
 
-        internal void Shutdown()
+        internal async Task ShutdownAsync()
         {
+            await SendMessageAsync("Goodbye cruel world").ConfigureAwait(false);
             _botClient.StopReceiving();
         }
 
         private async Task SendMessageAsync( string message )
         {
-            await _botClient.SendTextMessageAsync( Settings.CountingChatId, message ).ConfigureAwait( false );
+            await _botClient.SendTextMessageAsync( Settings.MetaCountingChatId, message ).ConfigureAwait( false );
         }
 
         private async void OnMessageReceivedAsync(object sender, MessageEventArgs e)
         {
-            await SendMessageAsync( $"{e.Message.Text}" ).ConfigureAwait( false );
+            if (e.Message.Chat.Id == Settings.CountingChatId)
+            {
+                if (int.TryParse(e.Message.Text, out int number))
+                {                
+                    if (lastNumber != null)
+                    {
+                        if (number == lastNumber + 1)
+                        {
+                            lastNumber = number;
+                        }
+                        else
+                        {
+                            await SendMessageAsync($"@{e.Message.From.Username}, wrong fucking number, fix it...or else").ConfigureAwait(false);
+                            lastNumber = null;
+                        }
+                    }
+                    else
+                    {
+                        lastNumber = number;   
+                    }
+                }
+                else
+                {
+                    await SendMessageAsync($"@{e.Message.From.Username}, holy shit, that's not even a number!").ConfigureAwait(false);
+                    lastNumber = null;
+                }
+            }
         }
         
-        private async void OnMessageEditedRecievedAysnc(object sender, MessageEventArgs e)
-        {
-            await SendMessageAsync( $"{e.Message.Text} was edited" ).ConfigureAwait( false );
-        }
+        // private async void OnMessageEditedRecievedAysnc(object sender, MessageEventArgs e)
+        // {
+        //     //await SendMessageAsync( $"{e.Message.Text} was edited" ).ConfigureAwait( false );
+        // }
     }
 }
