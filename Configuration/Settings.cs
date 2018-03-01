955b4161f3a959c44454c3aa5931d2cee007af86
TJ Kelly <tj.kelly0795@gmail.com>
2018-03-01 17:58:21 -0500

    refactor - added config and made the code digestable

---
+++ Configuration/Settings.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace TheCountBot.Configuration
+{  
+    public static class Settings
+    {
+        private static ISettingsProvider _settingsProvider = null;
+
+        public static string BotIdSecret => ThrowIfNotInitialized( "botIdSecret" );
+
+        public static int CountingChatId => Convert.ToInt32( ThrowIfNotInitialized( "countingChatId" ) );
+
+        public static void Initialize( ISettingsProvider settingsProvider )
+        {
+            _settingsProvider = settingsProvider;
+        }
+
+        private static string ThrowIfNotInitialized( string configurationKey )
+        {
+            if ( _settingsProvider == null )
+            {
+                throw new InvalidOperationException( "Configuration root has not been initialized. Call Settings.Initialize on app startup." );
+            }
+
+            return _settingsProvider.Retrieve( configurationKey );
+        }
+    }
+}
