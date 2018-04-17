46111231b2af0ba7fbf04db6317f984352b98f26
TJ Kelly <tj.kelly0795@gmail.com>
2018-04-17 19:22:05 -0400

    NO MORE SQL INJECTION

--- models/NumberStoreContext.cs
+++ models/NumberStoreContext.cs
@@ -24,8 +24,16 @@ namespace TheCountBot.Models
             using (MySqlConnection conn = GetConnection())
             {
                 await conn.OpenAsync().ConfigureAwait(false);
+                // '{record.Username}', {record.Number}, {record.Correct.ToString().ToUpper()}, '{record.Timestamp}'
+                MySqlCommand insertCommand = new MySqlCommand($"INSERT INTO NumberStore VALUES (@username, @number, @correct, @timestamp);", conn);
+                insertCommand.Parameters.AddWithValue("@username", record.Username);
+                insertCommand.Parameters.AddWithValue("@number",  record.Number);
+                insertCommand.Parameters.AddWithValue("@correct", record.Correct);
+                insertCommand.Parameters.AddWithValue("@timestamp", record.Timestamp);
 
-                MySqlCommand insertCommand = new MySqlCommand($"INSERT INTO NumberStore VALUES ('{record.Username}', {record.Number}, {record.Correct.ToString().ToUpper()}, '{record.Timestamp}');", conn);
+                System.Console.WriteLine(insertCommand);
+
+
                 int rowsAffected = await insertCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
                 
                 await conn.CloseAsync().ConfigureAwait(false);
