ea8a63a760b2f80c215a6cc50e820b430a61c810
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:43:54 -0500

    + TelegramBotClient implementation

---
+++ TheCountBot.Api/Implementations/TelegramBotClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using TheCountBot.Core.Interfaces;
+using ITelegramBotClientExternal = Telegram.Bot.ITelegramBotClient;
+
+namespace TheCountBot.Api.Implementations
+{
+    public class TelegramBotClient : ITelegramBotClient
+    {
+        private readonly ITelegramBotClientExternal _telegramBotClient;
+        private readonly Settings _settings;
+
+        public TelegramBotClient( ITelegramBotClientExternal telegramBotClient, IOptions<Settings> settingsOptions )
+        {
+            _telegramBotClient = telegramBotClient;
+            _settings = settingsOptions.Value;
+        }
+
+        public async Task SendRawMessageAsync( string message )
+        {
+            await _telegramBotClient.SendTextMessageAsync( _settings.MetaCountingChatId, message, Telegram.Bot.Types.Enums.ParseMode.Default );
+        }
+    }
+}
