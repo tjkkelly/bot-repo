8fb975edb675a8cba0beb9f022500dab36d7ab0f
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:49:14 -0500

    +NewMessageRequest

---
+++ TheCountBot.Api/Requests/NewMessageRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheCountBot.Api.Requests
+{
+    public class NewMessageRequest
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Number
+        {
+            get;
+            set;
+        }
+
+        public DateTime Timestamp
+        {
+            get;
+            set;
+        }
+    }
+}
