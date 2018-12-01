f3c8ddbd47b3f2d675ab251d0728a7fc44e251c7
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 11:18:03 -0500

    Add Api Project

---
+++ TheCountBot.Api/Settings.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace TheCountBot
+{
+    public class Settings
+    {
+        public string BotIdSecret{ get; set; }
+
+        public int CountingChatId{ get; set; }
+
+        public int MetaCountingChatId{ get; set; }
+
+        public int TimerWaitTime{ get; set; }
+
+        public List<string> InsultsForMessingUpTheNumber{ get; set; }
+
+        public string ConnectionString{ get; set; }
+
+        public string SqlConnectionStringReadWrite { get; set; }
+
+        public bool IsDebug { get; set; }
+    }
+}
