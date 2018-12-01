c6d708400a0a0a057401cd52996b51808c4787fa
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:45:30 -0500

    CustomizedInsultFactory

---
+++ TheCountBot.Core/Factories/CustomizedInsultFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheCountBot.Core.Factories
+{
+    public class CustomizedInsultFactory
+    {
+        private readonly IReadOnlyList<string> _templatedInsultList;
+        private readonly Random _rng;
+
+        public CustomizedInsultFactory( IReadOnlyList<string> templatedInsultList, Random rng )
+        {
+            _templatedInsultList = templatedInsultList;
+            _rng = rng;
+        }
+
+        public string GetInsultForUser( string username )
+        {
+            int _randInt = _rng.Next(0, _templatedInsultList.Count);
+            return _templatedInsultList[_randInt].Replace("{username}", username);
+        }
+    }
+}
