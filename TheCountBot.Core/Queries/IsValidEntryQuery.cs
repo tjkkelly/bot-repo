f3cba9d8335748f4f7564452936301aac1a79823
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:49:00 -0500

    + Queries and QueryHandlers

---
+++ TheCountBot.Core/Queries/IsValidEntryQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace TheCountBot.Core.Queries
+{
+    public class IsValidEntryQuery : IRequest<bool>
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Number
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ TheCountBot.Core/Queries/IsValidEntryQueryHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace TheCountBot.Core.Queries
+{
+    public class IsValidEntryQueryHandler : IRequestHandler<IsValidEntryQuery, bool>
+    {
+        private readonly PreviousMessageStateTracker _previousMessageStateTracker;
+
+        public IsValidEntryQueryHandler( PreviousMessageStateTracker previousMessageStateTracker )
+        {
+            _previousMessageStateTracker = previousMessageStateTracker;
+        }
+
+        public Task<bool> Handle( IsValidEntryQuery request, CancellationToken cancellationToken )
+        {
+            if ( NumberStartsWithZero( request.Number ) )
+            {
+                _previousMessageStateTracker.SetMessageStateToInvalid();
+                return Task.FromResult( false );
+            }
+
+            if ( !IsProperNumber( request.Number ) )
+            {
+                _previousMessageStateTracker.SetMessageStateToInvalid();
+                return Task.FromResult( false );
+            }
+
+            int number = int.Parse( request.Number );
+
+            if ( !_previousMessageStateTracker.IsUserNumberCompositionValid( request.Username, number ) )
+            {
+                _previousMessageStateTracker.SetMessageStateToInvalid();
+                return Task.FromResult( false );
+            }
+
+            _previousMessageStateTracker.SetMessageStateToValid( request.Username, number );
+            return Task.FromResult( true );
+        }
+
+        private bool NumberStartsWithZero( string number )
+        {
+            return number.StartsWith( "0", StringComparison.InvariantCulture );
+        }
+
+        private bool IsProperNumber( string number )
+        {
+            return int.TryParse( number, out int result );
+        }
+    }
+}
