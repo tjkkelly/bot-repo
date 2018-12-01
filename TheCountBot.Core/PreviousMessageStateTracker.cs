a5603c24aec353ab5104a9fd29f5d8c4750da4b3
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:44:43 -0500

    + StateTracker

---
+++ TheCountBot.Core/PreviousMessageStateTracker.cs
@@ -0,0 +1,41 @@
+﻿namespace TheCountBot.Core
+{
+    public class PreviousMessageStateTracker
+    {
+        private int? _lastNumber;
+        private string _lastUserToSendCorrectMessage;
+
+        public PreviousMessageStateTracker()
+        {
+            _lastNumber = null;
+            _lastUserToSendCorrectMessage = null;
+        }
+
+        public bool IsUserNumberCompositionValid( string username, int numberToCheck )
+        {
+            if ( _lastUserToSendCorrectMessage == username )
+            {
+                return false;
+            }
+
+            if ( numberToCheck != _lastNumber + 1 )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void SetMessageStateToInvalid()
+        {
+            _lastNumber = null;
+            _lastUserToSendCorrectMessage = null;
+        }
+
+        public void SetMessageStateToValid( string username, int number )
+        {
+            _lastNumber = number;
+            _lastUserToSendCorrectMessage = username;
+        }
+    }
+}
