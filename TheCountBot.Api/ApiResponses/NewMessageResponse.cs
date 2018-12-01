26aa9892bd62b5dab77762548865c953b627ea7a
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 14:50:05 -0500

    + MessageController

---
+++ TheCountBot.Api/ApiResponses/NewMessageResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TheCountBot.Api.ApiResponses
+{
+    public class NewMessageResponse
+    {
+        public string UserName { get; set; }
+     
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ TheCountBot.Api/Controllers/MessageController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheCountBot.Api.Requests;
+using MediatR;
+using TheCountBot.Core.Queries;
+using TheCountBot.Core.Commands;
+using TheCountBot.Api.ApiResponses;
+
+namespace TheCountBot.Api.Controllers
+{
+    [Route( "api/[controller]" )]
+    public class MessageController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public MessageController( IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<NewMessageResponse> NewCountingMessage( [FromBody] NewMessageRequest requestBody )
+        {
+            bool isCorrect = await _mediator.Send( new IsValidEntryQuery
+            {
+                Number = requestBody.Number,
+                Username = requestBody.Username
+            });
+
+            int number = 0;
+
+            if ( isCorrect )
+            {
+                number = int.Parse( requestBody.Number );
+            }
+
+            await _mediator.Send( new AddNewEntryCommand
+            {
+                Username = requestBody.Username,
+                Number = number,
+                IsCorrect = isCorrect,
+                Timestamp = requestBody.Timestamp
+            });
+
+            return new NewMessageResponse 
+            { 
+                UserName = requestBody.Username,
+                IsCorrect = isCorrect
+            };
+        }
+    }
+}
