b1833c6870bf2a040f671fa1cfd4ca3cb95c0617
TJ Kelly <tj.kelly0795@gmail.com>
2018-12-01 11:46:20 -0500

    Add error isup endpoint

---
+++ TheCountBot.Api/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace TheCountBot.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ErrorController : Controller
+    {
+        private readonly Settings _settings;
+
+        public ErrorController( IOptions<Settings> settings )
+        {
+            _settings = settings.Value;
+        }
+
+        [HttpGet, Route("isup")]
+        public string Get()
+        {
+            return $"[Is Debug: {_settings.IsDebug}] - OK";
+        }
+    }
+}
