62e99a4348732ce84f65af9687b3d888b1ed72d8
TJ Kelly <tj.kelly0795@gmail.com>
2019-03-03 11:55:41 -0500

    Add extension to convert from generated model and core model

---
+++ TheCountBot.Application/Clients/Models/UserStatisticsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TheCountBot.Application.Clients.Models
+{
+    public static class UserStatisticsExtensions
+    {
+        public static TheCountBot.Core.DataModels.UserStatistics ToCoreUserStatitics( this UserStatistics userStatistics )
+        {
+            return new Core.DataModels.UserStatistics
+            {
+                Username = userStatistics.Username,
+                MessagesSentCount = userStatistics.MessagesSentCount ?? 0,
+                TotalMessagePercentage = userStatistics.TotalMessagePercentage ?? 0,
+                TotalErrorRate = userStatistics.TotalErrorRate ?? 0,
+                ErrorRate = userStatistics.ErrorRate ?? 0,
+                RelativeErrorRate = userStatistics.RelativeErrorRate ?? 0,
+                MistakeCount = userStatistics.MistakeCount ?? 0
+            };
+        }
+    }
+}
